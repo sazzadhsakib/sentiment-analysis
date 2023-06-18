e4cf6968b42fb9c81ed378439739605bf3bd18ce
sazzadsakib <sazzad.sakib@cellosco.pe>
2023-06-19 01:00:33 +0600

    fix: request prompt

--- src/presentation/route/sentiment_analyzer_route.py
+++ src/presentation/route/sentiment_analyzer_route.py
@@ -6,7 +6,10 @@ from src.usecases.detect_sentiment_uc import DetectSentimentUC
 router = APIRouter(prefix='/detect')
 
 @router.post('/detect', response_model=DetectSentimentResponseModel)
-async def detectSentiment(detectSentimentRequestModel: DetectSentimentRequestModel, text: str = Body(...),):
-    detectSentimentRequestModel.text = text
+async def detectSentiment(
+    request: DetectSentimentRequestModel,
+):
+    detectSentimentRequestModel = DetectSentimentRequestModel(text=request.text)
     usecase = DetectSentimentUC()
     return await usecase(detectSentimentRequestModel)
+    
